b27448b10c5b39d1a521a602bf84720d154f64ff
LucileDeloubes <lucile.deloubes@gmail.com>
2020-11-18 17:36:33 +0100

    fin partie 3

--- index_p3_exo6.php
+++ index_p3_exo6.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Part 3 exo 5</title>
+    <title>PHP Part 3 exo 6</title>
 </head>
 
 <!-- Créer un tableau associatif departments avec comme index le numéro des départements des Hauts de France et en valeur leur nom. -->
@@ -14,8 +14,8 @@
 <?php 
     $department = array('02'=> 'Aisne', '59'=> 'Nord', '60'=> 'Oise', '62'=> 'Pas-de-Calais', '80'=> 'Somme')
 ?>
-<?php foreach($department as $element){ ?>
-<p><?php  echo $element  ?></p> <!-- clé affiche le premier élement et l'element affiche la valeur affecté -->
+<?php foreach($department as $cle => $element){ ?>
+<p><?php echo 'le département '. $element . ' a le numéro '. $cle ?></p> <!-- clé affiche le premier élement et l'element affiche la valeur affecté -->
 <?php } ?>
 <!-- le symbole => veut dire "associé à" d'où le terme tableau associatif-->
 
